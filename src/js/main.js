1b17acddc46127d5f88334eb713c22513284a61e
Agustín García <agustin.tavoite@gmail.com>
2022-04-11 11:00:33 +0200

    shipping form

--- src/js/main.js
+++ src/js/main.js
@@ -95,3 +95,20 @@ function muestraProductos(){
 }
 
 muestraProductos();
+
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("gift");
+    
+    // Get the output text
+    var text = document.getElementById("text");
+  var tit = document.getElementById("tit");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+      tit.style.display = "block";
+    } else {
+        text.style.display = "none";
+        tit.style.display = "none";
+    }
+  }
